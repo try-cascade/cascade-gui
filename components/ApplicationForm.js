cdc9752e3e4bec4db2101c18a5b5af1854427d17
Anne Tiotuico <amtiotuico@gmail.com>
2022-11-05 12:35:56 -0700

    styled application page

--- components/ApplicationForm.js
+++ components/ApplicationForm.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import styles from '../styles/Setup.module.css'
 
 const ApplicationForm = ( { onSubmit, appName } ) => {
   // const [name, setName] = useState('')
@@ -25,13 +26,27 @@ const ApplicationForm = ( { onSubmit, appName } ) => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        What would you like your application to be called?
-        <input type="text" value={appName[0]} onChange={handleChange}/>
-      </label>
-      <input type="submit" />
-    </form>
+    <>
+      <div className={styles.progress}>
+        <span className={`${styles.dot} ${styles.selected}`}>1</span>
+        <span>- - - - -</span>
+        <span className={styles.dot}>2</span>
+        <span>- - - - -</span>
+        <span className={styles.dot}>3</span>
+      </div>
+      <div className={styles.mainContent}>
+        <h1 className={styles.h1}>Create an Application</h1>
+        <p className={styles.p}>Let's get you started!</p> 
+        <p className={styles.p}>We will create a bucket in your AWS account to store all the necessary information for your application.</p>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <label className={styles.label}>
+            Application Name:
+            <input className={styles.input} type="text" value={appName[0]} onChange={handleChange}/>
+          </label>
+          <input className={styles.button} type="submit" />
+        </form>
+      </div>
+    </>
   )
 }
 
