0030261203007fb6d34ed884e682ce5c4f4975a7
NatalieAThompson <60165247+NatalieAThompson@users.noreply.github.com>
2022-11-09 19:33:53 -0600

    Update event.js

--- utils/event.js
+++ utils/event.js
@@ -15,45 +15,44 @@ export const streamTfData = async (path, action, setBool, dispatch) => {
      dispatch({type: "vpc", payload: setBool})
     } else if (event.data.includes(`(cs-${envName}-table): ${action} complete`)) {
       dispatch({type: "route table", payload: setBool})
+    } else if (event.data.includes(`(cs-${envName}-internet-gateway): ${action} complete`)) {
+      dispatch({type: "gateway", payload: setBool})
+    } else if (event.data.includes(`(cs-${envName}-route): ${action} complete`)) {
+      dispatch({type: "route", payload: setBool})
+    } else if (event.data.includes(`(cs-${envName}-public-1): ${action} complete`)) {
+      dispatch({type: "public subnet 1", payload: setBool})
+    } else if (event.data.includes(`(cs-${envName}-public-2): ${action} complete`)) {
+       dispatch({type: "public subnet 2", payload: setBool})
+    } else if (event.data.includes(`(cs-${envName}-sub-assoc-1): ${action} complete`)) {
+      dispatch({type: "route table association 1", payload: setBool})
+    } else if (event.data.includes(`(cs-${envName}-sub-assoc-2): ${action} complete`)) {
+      dispatch({type: "route table association 2", payload: setBool})
     }
-    // } else if (event.data.includes(`(cs-${envName}-internet-gateway): ${action} complete`)) {
-    //   dispatch({type: "gateway", payload: setBool})
-    // } else if (event.data.includes(`(cs-${envName}-route): ${action} complete`)) {
-    //   dispatch({type: "route", payload: setBool})
-    // } else if (event.data.includes(`(cs-${envName}-public-1): ${action} complete`)) {
-    //   dispatch({type: "public subnet 1", payload: setBool})
-    // } else if (event.data.includes(`(cs-${envName}-public-2): ${action} complete`)) {
-    //    dispatch({type: "public subnet 2", payload: setBool})
-    // } else if (event.data.includes(`(cs-${envName}-sub-assoc-1): ${action} complete`)) {
-    //   dispatch({type: "route table association 1", payload: setBool})
-    // } else if (event.data.includes(`(cs-${envName}-sub-assoc-2): ${action} complete`)) {
-    //   dispatch({type: "route table association 2", payload: setBool})
-    // }
-    // // service stack
-    //   else if (event.data.includes(`(ecs--cs-${envName}-loggroup): ${action} complete`)) {
-    //   dispatch({type: "log group", payload: setBool})
-    // } else if (event.data.includes(`(cs-${envName}-target-group): ${action} complete`)) {
-    //   dispatch({type: "alb target group", payload: setBool})
-    // } else if (event.data.includes(`(cs-${envName}-task-role): ${action} complete`)) {
-    //   dispatch({type: "task role", payload: setBool})
-    // } else if (event.data.includes(`(cs-${envName}-execution-role): ${action} complete`)) {
-    //   dispatch({type: "execution role", payload: setBool})
-    // } else if (event.data.includes(`(cs-${envName}-task-definition): ${action} complete`)) {
-    //   dispatch({type: "task definition", payload: setBool})
-    // } else if (event.data.includes(`(cs-${envName}-security-group): ${action} complete`)) {
-    //   dispatch({type: "security group", payload: setBool})
-    // } else if (event.data.includes(`(cs-${envName}-alb-security-group): ${action} complete`)) {
-    //   dispatch({type: "alb security group", payload: setBool})
-    // } else if (event.data.includes(`(cs-${envName}-cluster): ${action} complete`)) {
-    //   dispatch({type: "ecs cluster", payload: setBool})
-    // } else if (event.data.includes(`(cs-${envName}-lb): ${action} complete`)) {
-    //   // setLoadBalancer(setBool)
-    //   dispatch({type: "load balancer", payload: setBool})
-    // } else if (event.data.includes(`(cs-${envName}-alb-listener): ${action} complete`)) {
-    //   dispatch({type: "alb listener", payload: setBool})
-    // } else if (event.data.includes(`(cs-${envName}-service): ${action} complete`)) {
-    //   dispatch({type: "ecs service", payload: setBool})
-    // }
+    // service stack
+      else if (event.data.includes(`(ecs--cs-${envName}-loggroup): ${action} complete`)) {
+      dispatch({type: "log group", payload: setBool})
+    } else if (event.data.includes(`(cs-${envName}-target-group): ${action} complete`)) {
+      dispatch({type: "alb target group", payload: setBool})
+    } else if (event.data.includes(`(cs-${envName}-task-role): ${action} complete`)) {
+      dispatch({type: "task role", payload: setBool})
+    } else if (event.data.includes(`(cs-${envName}-execution-role): ${action} complete`)) {
+      dispatch({type: "execution role", payload: setBool})
+    } else if (event.data.includes(`(cs-${envName}-task-definition): ${action} complete`)) {
+      dispatch({type: "task definition", payload: setBool})
+    } else if (event.data.includes(`(cs-${envName}-security-group): ${action} complete`)) {
+      dispatch({type: "security group", payload: setBool})
+    } else if (event.data.includes(`(cs-${envName}-alb-security-group): ${action} complete`)) {
+      dispatch({type: "alb security group", payload: setBool})
+    } else if (event.data.includes(`(cs-${envName}-cluster): ${action} complete`)) {
+      dispatch({type: "ecs cluster", payload: setBool})
+    } else if (event.data.includes(`(cs-${envName}-lb): ${action} complete`)) {
+      // setLoadBalancer(setBool)
+      dispatch({type: "load balancer", payload: setBool})
+    } else if (event.data.includes(`(cs-${envName}-alb-listener): ${action} complete`)) {
+      dispatch({type: "alb listener", payload: setBool})
+    } else if (event.data.includes(`(cs-${envName}-service): ${action} complete`)) {
+      dispatch({type: "ecs service", payload: setBool})
+    }
 
     console.log(event.data, "<--- event data");
   };
