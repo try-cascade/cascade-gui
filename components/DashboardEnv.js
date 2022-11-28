b83f14329d8b4322ea11eef25551dcd8da1614df
Natalie Thompson <Natalie.A.Thompson@outlook.com>
2022-11-28 11:39:07 -0600

    Formatted resources into a more descriptive format
    Co-authored-by: Anne Tiotuico <amtiotuico@gmail.com>
    Co-authored-by: Rona Hsu <ronahsu@gmail.com>
    Co-authored-by: Yueun Kim <yueun.owl@gmail.com>

--- components/DashboardEnv.js
+++ components/DashboardEnv.js
@@ -1,5 +1,27 @@
 import styles from '../styles/Dashboard.module.css'
 
+const resourceTitles = {
+  "vpc": "Virtual Private Cloud",
+  "routeTable": "Route Table",
+  "gateway": "Internet Gateway",
+  "route": "Route",
+  "pubSub1": "Public Subnet 1",
+  "pubSub2": "Public Subnet 2",
+  "routeTableAssoc1": "Route Table Association 1",
+  "routeTableAssoc2": "Route Table Association 2",
+  "logGroup": "CloudWatch Log Group",
+  "albTargetGroup": "Target Group for Application Load Balancer",
+  "executionRole": "IAM Execution Role",
+  "taskDefinition": "ECS Task Definition",
+  "securityGroup": "ECS Cluster Security Group",
+  "ecsCluster": "ECS Cluster",
+  "albSecurityGroup": "Application Load Balancer Security Group",
+  "ecsService": "ECS Service",
+  "taskRole": "IAM Task Role",
+  "loadBalancer": "Application Load Balancer",
+  "albListener": "Application Load Balancer Listener"
+}
+
 const DashboardEnv = ({ state, envName }) => {
   return (
     <div className={styles.envDetails}>
@@ -30,7 +52,7 @@ const DashboardEnv = ({ state, envName }) => {
                       { state.post[stack][resources] !== "loading" ?
                       <div className='circle'><span className={state.post[stack][resources]}></span></div> :
                       <span className={state.post[stack][resources]}></span> }
-                      {resources}
+                      {resourceTitles[resources]}
                     </li>
                   )
                 })}
